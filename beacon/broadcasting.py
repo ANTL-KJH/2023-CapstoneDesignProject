eab39a91fa9afa14204538db06de4f5a02ec006e
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2023-07-02 21:40:45 -0400

    Update broadcasting.py

--- beacon/broadcasting.py
+++ beacon/broadcasting.py
@@ -13,6 +13,8 @@
 import os
 
 def main():
+    os.system("sudo hciconfig hci0 up")
+    os.system("sudo hciconfig hcio leadv 3")
     os.system("sudo hcitool -i hci0 cmd 0x08 0x0008 13 02 01 06 03 03 aa fe 0b 16 aa fe 10 00 74 72 66 52 36 30 00 00 00 00 00 00 00 00 00 00 00 00")
 
 if __name__ == "__main__":
